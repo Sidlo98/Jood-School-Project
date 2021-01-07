bf946582fc089d89e40baf1cba45eab19770f072
Sidlo98 <poppe9898@hotmail.com>
2021-01-07 17:00:15 +0100

    application,download

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 new WOW().init();
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  slidesPerView: 1,
+      spaceBetween: 10,
+      loop:true,
+      centeredSlides:true,
+      breakpoints: {
+        900:{
+          slidesPerView: 3,
+          spaceBetween: 30,
+         },
+       1400: {
+        slidesPerView: 5,
+        spaceBetween: 30,
+       }
+       
+      }
+  
+})
